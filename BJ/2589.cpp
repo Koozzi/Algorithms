dc0fd2d872668e2f2da1c12d215ccf8f234bd524
Koozzi <kchy666@naver.com>
2020-02-11 15:14:06 +0900

    20200211

---
+++ BJ/2589.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <queue>
+#include <memory.h>
+
+using namespace std;
+
+typedef struct{
+    int moveI, moveJ;
+}Dir;
+Dir moveDir[4] = {{-1,0}, {1,0}, {0,1}, {0,-1}};
+
+int depth[50][50];
+char map[51][51];
+bool visited[50][50];
+
+int I, J;
+
+int BFS(int startI, int startJ){
+    memset(visited, false, sizeof(visited));
+    memset(depth, 0, sizeof(depth));
+    int deep = 0;
+    queue<pair<int, int>> q;
+    q.push({startI, startJ});
+    visited[startI][startJ] = true;
+    while(!q.empty()){
+        int currentI = q.front().first;
+        int currentJ = q.front().second;
+        q.pop();
+        for(int i = 0 ; i < 4 ; i++){
+            int nextI = currentI + moveDir[i].moveI;
+            int nextJ = currentJ + moveDir[i].moveJ;
+            if(nextI >= 0 && nextI < I && nextJ >= 0 && nextJ < J){
+                if(!visited[nextI][nextJ] && map[nextI][nextJ] == 'L'){
+                    q.push({nextI, nextJ});
+                    visited[nextI][nextJ] = true;
+                    depth[nextI][nextJ] = depth[currentI][currentJ] + 1;
+                    if(depth[nextI][nextJ] > deep){
+                        deep = depth[nextI][nextJ];
+                    }
+                }
+            }
+        }
+    }
+    return deep;
+}
+
+int main(){
+    int longest = 0;
+    cin >> I >> J;
+    for(int i = 0 ; i < I ; i++){
+        cin >> map[i];
+    }
+    for(int i = 0 ; i < I ; i++){
+        for(int j = 0 ; j < J ; j++){
+            if(map[i][j] == 'L'){
+                if(BFS(i, j) >= longest){
+                    longest = BFS(i, j);
+                }
+            }
+        }
+    }
+    cout << longest << "\n"
+    ;
+    return 0;
+}
