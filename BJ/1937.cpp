a8d5d5cd0be0491b863329437876aa6e3b49f544
Koozzi <kchy666@naver.com>
2020-03-19 03:03:49 +0900

    20200319

--- BJ/1937.cpp
+++ BJ/1937.cpp
@@ -1,47 +1,39 @@
 #include <iostream>
 #include <queue>
 #include <algorithm>
-#include <vector>
+#include <cstring>
 
 using namespace std;
+
+int M;
+int map[500][500];
+int dp[500][500];
 
 typedef struct{
     int moveI, moveJ;
 }Dir;
 Dir moveDir[4] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
 
-int M;
-int map[500][500];
-int dp[500][500]; 
-void show(){
-    cout << endl;
-    for(int i = 0 ; i < M ; i++){
-        for(int j = 0 ; j < M ; j++){
-            cout << dp[i][j] << " ";
-        }cout << endl;
+int move(int startI, int startJ){
+    if(dp[startI][startJ] != 0){
+        return dp[startI][startJ];
     }
-}
-int DFS(int startI, int startJ){
-    bool flag = false;
-    vector<pair<int, int>> v;
+    dp[startI][startJ] = 1;
     for(int i = 0 ; i < 4 ; i++){
         int nextI = startI + moveDir[i].moveI;
         int nextJ = startJ + moveDir[i].moveJ;
         if(nextI >= 0 && nextI < M && nextJ >= 0 && nextJ < M){
-            if(map[nextI][nextJ] > map[startI][startJ]){
-                flag = true;
-                dp[startI][startJ] = max(dp[startI][startJ], DFS(nextI, nextJ) + 1);
+            if(map[startI][startJ] < map[nextI][nextJ]){
+                dp[startI][startJ] = max(dp[startI][startJ], move(nextI, nextJ) + 1);
             }
         }
     }
-    if(!flag){
-        return 1;
-    }
-    else{
-        return startD;
-    }
+    return dp[startI][startJ];
 }
+
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
     cin >> M;
     for(int i = 0 ; i < M ; i++){
         for(int j = 0 ; j < M ; j++){
@@ -51,59 +43,9 @@ int main(){
     int ans = 0;
     for(int i = 0 ; i < M ; i++){
         for(int j = 0 ; j < M ; j++){
-            ans = max(ans, DFS(i,j,1));
+            ans = max(ans, move(i,j));
         }
     }
     cout << ans << endl;
     return 0;
 }
-
-/*
-4
-14 9 12 10
-1 11 5 4
-7 15 2 13
-6 3 16 8
--> 4
-
-2
-1 1
-1 1
--> 1
-
-2
-1 2
-2 4
--> 3
-
-5
-1 1 1 1 1
-1 2 2 2 1
-1 2 3 2 1
-1 2 2 2 1
-1 1 1 1 1
--> 3
-
-5
-3 3 3 3 3
-3 2 2 2 3
-3 2 1 2 3
-3 2 2 2 3
-3 3 3 3 3
--> 3
-
-5
-3 3 3 4 3
-3 2 2 2 3
-3 2 1 2 3
-3 2 2 2 3
-3 3 3 3 3
--> 4
-
-4 
-14 9 12 10
-1 11 5 4
-10 15 4 5
-9 8 7 6
--> 8
-*/
