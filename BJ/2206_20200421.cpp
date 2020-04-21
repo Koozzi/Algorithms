46f307d15b0f392a094119d6f9fe8ce83ac0b166
koozzi <kchy666@naver.com>
2020-04-22 00:56:41 +0900

    20200422

--- BJ/2206_20200421.cpp
+++ BJ/2206_20200421.cpp
@@ -14,7 +14,7 @@ Dir moveDir[4] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
 
 int BFS(){
     queue<pair<pair<int, int>, pair<int, int>>> q;
-    q.push({{0,0},{0,0}});
+    q.push({{0,0},{1,0}});
     visited[0][0][0] = true;
     while(!q.empty()){
         int currentI = q.front().first.first; 
@@ -49,6 +49,10 @@ int BFS(){
 }
 
 int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
     cin >> M >> N;
     for(int i = 0 ; i < M ; i++){
         cin >> map[i];
---
+++ BJ/3055_20200422.cpp
@@ -0,0 +1,107 @@
+#include <iostream>
+#include <string>
+#include <queue>
+using namespace std;
+
+string map[50];
+int M, N, startI, startJ, destI, destJ;
+bool visited[50][50];
+
+queue<pair<int,int>> water;
+queue<pair<pair<int, int>, int>> q;
+
+typedef struct{
+    int moveI, moveJ;
+}Dir;
+Dir moveDir[4] = {{0,1}, {0,-1}, {1,0}, {-1,0}};
+
+void show(){
+    cout << "\n";
+    for(int i = 0 ; i < M ; i++){
+        for(int j = 0 ; j < N ; j++){
+            cout << map[i][j];
+        }cout << "\n";
+    }
+}
+
+void flood(){
+    int waterSize = water.size();
+    for(int i = 0 ; i < waterSize ; i++){
+        int currentI = water.front().first;
+        int currentJ = water.front().second;
+        water.pop();
+
+        for(int j = 0 ; j < 4 ; j++){
+            int nextI = currentI + moveDir[j].moveI;
+            int nextJ = currentJ + moveDir[j].moveJ;
+
+            if(nextI < 0 || nextJ < 0 || nextI >= M || nextJ >= N){
+                continue;
+            }
+
+            if(map[nextI][nextJ] == '.' || map[nextI][nextJ] == 'S'){
+                map[nextI][nextJ] = '*';
+                water.push({nextI, nextJ});
+            }
+        }
+    }
+}
+
+void BFS(){
+    int depthCheck = 0;
+    q.push({{startI, startJ}, 0});
+    while(!q.empty()){
+        int currentI = q.front().first.first;
+        int currentJ = q.front().first.second;
+        int depth = q.front().second;
+        q.pop();
+
+        if(depthCheck != depth){
+            depthCheck++;
+            flood();
+            show();
+        }
+
+        for(int i = 0 ; i < 4 ; i++){
+            int nextI = currentI + moveDir[i].moveI;
+            int nextJ = currentJ + moveDir[i].moveJ;
+
+            if(nextI < 0 || nextJ < 0 || nextI >= M || nextJ >= N){
+                continue;
+            }
+
+            if(map[nextI][nextJ] == 'D' && map[currentI][currentJ] == 'S'){
+                cout << depth+1 << "\n";
+                exit(0);
+            }
+
+            if(map[nextI][nextJ] == '.'){
+                q.push({{nextI, nextJ}, depth+1});
+                map[nextI][nextJ] = 'S';
+            }
+        }
+    }
+}
+
+int main(){
+    cin >> M >> N;
+    for(int i = 0 ; i < M ; i++){
+        cin >> map[i];
+        for(int j = 0 ; j < N ; j++){
+            if(map[i][j] == 'D'){
+                destI = i;
+                destJ = j;
+            }
+            else if(map[i][j] == 'S'){
+                startI = i;
+                startJ = j;
+            }
+            else if(map[i][j] == '*'){
+                water.push({i,j});
+            }
+        }
+    }
+    BFS();
+    cout << "KAKTUS" << "\n";
+    return 0;
+}
