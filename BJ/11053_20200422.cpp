a5092a7340209756ea324870cdbff3344a0f4c78
koozzi <kchy666@naver.com>
2020-04-22 15:22:29 +0900

    20200422

---
+++ BJ/11053_20200422.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+int M, arr[1000];
+vector<int> v;
+int main(){
+    cin >> M;
+    for(int i = 0 ; i < M ; i++){
+        cin >> arr[i];
+    }
+    v.push_back(arr[0]);
+    for(int i = 1 ; i < M ; i++){
+        if(v.back() < arr[i]){
+            v.push_back(arr[i]);
+        }
+        else{
+            vector<int>::iterator iter = lower_bound(v.begin(), v.end(), arr[i]);
+            v[iter - v.begin()] = arr[i];
+        }
+    }
+    cout << v.size() << "\n";
+    return 0;
+}
--- BJ/2156_20200422.cpp
+++ BJ/2156_20200422.cpp
@@ -6,15 +6,18 @@ using namespace std;
 int M, ans, dp[MAX_NUM], arr[MAX_NUM];
 
 int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
     cin >> M;
     for(int i = 1 ; i < M+1 ; i++){
         cin >> arr[i];
     }
     dp[1] = arr[1];
     dp[2] = arr[1] + arr[2];
-    dp[3] = max(arr[1], arr[2]) + arr[3];
 
-    for(int i = 4 ; i < M+1 ; i++){
+    for(int i = 3 ; i < M+1 ; i++){
         dp[i] = max(dp[i-1], max(dp[i-2], dp[i-3] + arr[i-1]) + arr[i]);
     }
 
