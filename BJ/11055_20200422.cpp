7d6e851803a0cb133bd1ec113fecf8b780f145ce
koozzi <kchy666@naver.com>
2020-04-22 16:10:02 +0900

    20200422

---
+++ BJ/11055_20200422.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int M, arr[1000], dp[1000];
+
+int main(){
+    cin >> M;
+    for(int i = 0 ; i < M ; i++){
+        cin >> arr[i];
+    }
+
+    dp[0] = arr[0];
+    if(arr[1] > arr[0]){
+        dp[1] = arr[0] + arr[1];
+    }else{
+        dp[1] = arr[1];
+    }
+
+    for(int i = 2 ; i < M ; i++){
+        dp[i] = arr[i];
+        for(int j = 0 ; j < i ; j++){
+            if(arr[j] < arr[i]){
+                dp[i] = max(dp[i], dp[j] + arr[i]);
+            }
+        }
+    }
+
+    int ans = 0;
+    for(int i = 0 ; i < M ; i++){
+        ans = max(ans, dp[i]);
+    }
+    cout << ans << "\n";
+    return 0;
+}
