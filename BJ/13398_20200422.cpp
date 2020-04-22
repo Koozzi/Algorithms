173b4e2ca22a0c1e9363c082c03805830eeda367
koozzi <kchy666@naver.com>
2020-04-22 16:44:05 +0900

    20200422

--- BJ/13398_20200422.cpp
+++ BJ/13398_20200422.cpp
@@ -6,13 +6,15 @@ using namespace std;
 int M, ans, dp[MAX_NUM][2], arr[MAX_NUM];
 
 int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
     cin >> M;
     for(int i = 0 ; i < M ; i++){
         cin >> arr[i];
     }
     
-    dp[0][0] = arr[0];
-    ans = max(dp[0][1], dp[0][0]);
+    ans = dp[0][0] = arr[0];
 
     for(int i = 1 ; i < M ; i++){
         dp[i][0] = max(dp[i-1][0] + arr[i], arr[i]);
