0244b714a8a6ec41f3f9c8a5c4ef184726178902
koozzi <kchy666@naver.com>
2020-04-23 23:14:49 +0900

    20200423

--- BJ/12865_20200423.cpp
+++ BJ/12865_20200423.cpp
@@ -1,36 +1,35 @@
 #include <iostream>
 #include <algorithm>
-#include <vector>
 #define MAX_NUM 100001
 using namespace std;
 
 int M, K, ans;
+int arr[101][2];
 int dp[101][MAX_NUM];
-vector<pair<int, int>> v;
 
 int main(){
     ios_base::sync_with_stdio(0);
     cin.tie(0);
     cout.tie(0);
+
     cin >> M >> K;
+    
     v.push_back({0,0});
-    for(int i = 0 ; i < M ; i++){
-        int a, b;
-        cin >> a >> b;
-        v.push_back({a,b});
+    for(int i = 1 ; i <= M ; i++){
+        cin >> arr[i][0] >> arr[i][1];
     }
     
     for(int i = 1 ; i <= K ; i++){
-        if(i >= v[1].first){
-            dp[1][i] = v[1].second;
+        if(i >= arr[1][0]){
+            dp[1][i] = arr[1][1];
         }
     }
 
     for(int i = 2 ; i <= M ; i++){
         for(int j = 1 ; j <= K ; j++){
             dp[i][j] = dp[i-1][j];
-            int weight = v[i].first;
-            int value = v[i].second;
+            int weight = arr[i][0];
+            int value = arr[i][1];
             if(j >= weight){
                 dp[i][j] = max(dp[i-1][j], dp[i-1][j-weight] + value);
                 ans = max(ans, dp[i][j]);
