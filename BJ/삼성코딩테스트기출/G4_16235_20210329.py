bbfbc5a6ed804d5e2652400872c6c47c49cef70d
koozzi <koozzi666@gmail.com>
2021-03-29 22:48:50 +0900

    16235_20210329

---
+++ BJ/삼성코딩테스트기출/G4_16235_20210329.py
@@ -0,0 +1,65 @@
+# 21:40
+# 22:46
+#
+
+def spring_summer():
+    dead_tree = []
+    for i in range(N):
+        for j in range(N):
+            if not tree_board[i][j]: continue
+            tree_board[i][j].sort()
+            for idx, tree_age in enumerate(tree_board[i][j]):
+                if tree_age <= food[i][j]:
+                    food[i][j] -= tree_age
+                    tree_board[i][j][idx] += 1
+                else:
+                    for dead_tree_age in tree_board[i][j][idx:]:
+                        dead_tree.append([i,j,dead_tree_age])
+                    tree_board[i][j] = tree_board[i][j][:idx]
+                    break
+
+    for I, J, A in dead_tree:
+        food[I][J] += A // 2
+
+def fall():
+    for i in range(N):
+        for j in range(N):
+            for tree_age in tree_board[i][j]:
+                if tree_age % 5 == 0:
+                    for move in move_dir:
+                        next_i = i + move[0]
+                        next_j = j + move[1]
+
+                        if not 0 <= next_i < N or not 0 <= next_j < N:
+                            continue
+                        
+                        tree_board[next_i][next_j].append(1)
+
+def winter():
+    for i in range(N):
+        for j in range(N):
+            food[i][j] += add_food[i][j]
+
+if __name__=="__main__":
+
+    answer = 0
+    N, M, K = map(int, input().split())
+    add_food = [list(map(int, input().split())) for _ in range(N)]
+    tree_board = [[[] for _ in range(N)] for _ in range(N)]
+    food = [[5 for _ in range(N)] for _ in range(N)]
+    move_dir = [[0,1],[1,1],[1,0],[1,-1],[0,-1],[-1,-1],[-1,0],[-1,1]]
+
+    for _ in range(M):
+        I, J, A = map(int, input().split())   
+        tree_board[I-1][J-1].append(A)
+
+    for _ in range(K):
+        spring_summer()
+        fall()
+        winter()
+
+    for i in range(N):
+        for j in range(N):
+            answer += len(tree_board[i][j])
+
+    print(answer)
